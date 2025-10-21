6ec046dce9f18d0cb6c689b8aaeb9abb87eb255f
Mahdi Baiteh <mahdibraiteh@gmail.com>
2025-10-20 21:58:47 -0500

    Test jenkins file trigger 14

--- test/specs/test1.spec.js
+++ test/specs/test1.spec.js
@@ -1,7 +1,7 @@
 describe('Demo test 1', function(){
     it('my first test', async () => { // or we can say () => instead of function()
         browser.url('https://google.com/')
-        await $('[name1="q"]').setValue('WebdriverIo')
+        await $('[name="q"]').setValue('WebdriverIo')
         //await $('[name="btnK"]').click()
         browser.keys('Enter')
         browser.pause(4000)
